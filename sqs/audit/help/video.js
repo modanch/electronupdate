fd30917f49c58e6b9dd7b4e22fefa7d64625f3e3
Stefan Hintermeister <stuwi@stuwi.ch>
2019-08-27 14:42:00 +0200

    no back

--- sqs/audit/help/video.js
+++ sqs/audit/help/video.js
@@ -2,8 +2,8 @@ function startVideo() {
 
     var url = new URL(window.location.href);
 
-    var l = url.searchParams.get("lang");
-    $("#backlink").attr("href", "index.html?lang=" + l);
+    //var l = url.searchParams.get("lang");
+    //$("#backlink").attr("href", "index.html?lang=" + l);
 
     var c = url.searchParams.get("video");
     $("#videoctrl").attr("src", c);
