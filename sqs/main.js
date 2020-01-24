fb847e066d342ae28b668b873290f687dc2321ce
Stefan Hintermeister <stuwi@stuwi.ch>
2020-01-24 10:49:19 +0100

    sieht sonst Scheisse aus

--- sqs/main.js
+++ sqs/main.js
@@ -4,10 +4,10 @@ function preparePage(noRefresh) {
         $("body").prepend(result);
 
         var href = document.location.href;
-        if (href.indexOf("/status") > 0) 
-            $("#nv_status").hide();
-        if (href.indexOf("/versions") > 0)
-            $("#nv_versionen").hide();
+        // if (href.indexOf("/status") > 0) 
+        //     $("#nv_status").hide();
+        // if (href.indexOf("/versions") > 0)
+        //     $("#nv_versionen").hide();
 
         if (noRefresh == true)
             $("#nav_refresh").hide();
